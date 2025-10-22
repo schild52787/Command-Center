c41f67499fc0fa28aeb024f21d04298a456f1832
schild52787 <kyle.schildkraut@gmail.com>
2025-10-22 14:35:05 -0500

    Refactor Babel plugins for stricter transformation

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,11 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['@babel/plugin-transform-private-methods'],
+    plugins: [
+      ['@babel/plugin-transform-private-methods', { loose: false }],
+      ['@babel/plugin-transform-class-properties', { loose: false }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: false }],
+    ],
   };
 };
+
